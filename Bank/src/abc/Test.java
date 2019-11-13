16a4136e75c94770c5431818d49ca37c4749b022
zyf-123 <1441766130@qq.com>
2019-11-13 11:51:46 +0800

    set get

--- Bank/src/abc/Test.java
+++ Bank/src/abc/Test.java
@@ -1,24 +1,59 @@
 package abc;
 
 public class Test {
-	String id;
-	int card;
-	int balance;
-	
+	private String id;
+	private int card;
+	private int balance;
+
+	public void setid(String id)
+	{
+		this.id=id;
+	}	
+	public String getid()
+	{
+		return id;
+	}
+	public void setcard(int card)
+	{
+		this.card=card;
+	}	
+	public int getcard()
+	{
+		return card;
+	}
+	public void setbalance(int balance)
+	{
+		this.balance=balance;
+	}	
+	public int getbalance()
+	{
+		return balance;
+	}
 	Test(String n,int b,int c)
 	{
 		id=n;
 		card=b;
 		balance=c;
 	}
+	
 	int Add(int c)
 	{
 		balance=balance+c;
 		return balance;
 	}
 	int Reduce(int d)
+	
 	{
-		balance=balance-d;
-		return balance;
+		if(d>balance)
+			{
+			System.out.println("Óà¶î²»×ã");
+			return 0;
+			}
+		
+			
+			balance=balance-d;
+			
+			return balance;
+			
 	}
 }
--- Bank/src/abc/tt.java
+++ Bank/src/abc/tt.java
@@ -9,11 +9,11 @@ public class tt {
 		
 	Test j = new Test("zyf",1000,0);
      j.Add(1000);
-     System.out.println("Ê£Óà½ğ¶î"+j.balance);
+     System.out.println("Ê£Óà½ğ¶î"+j.getbalance());
      j.Reduce(500);
 
 
-	System.out.println("Ê£ÓàÓà¶îÊÇ"+j.balance);
+	System.out.println("Ê£ÓàÓà¶îÊÇ"+j.getbalance());
 	}
 
 		
